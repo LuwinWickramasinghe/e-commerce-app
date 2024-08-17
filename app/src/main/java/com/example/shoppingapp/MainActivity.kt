7568c49205a0db6770b3f3bba3d71d119076fdc8
LuwinWickramasinghe <luwin.anuka@gmail.com>
2024-08-17 23:13:22 +0530

    Get Data from Json and displayed on the screen. (Retrofit Documentations)

--- app/src/main/java/com/example/shoppingapp/MainActivity.kt
+++ app/src/main/java/com/example/shoppingapp/MainActivity.kt
@@ -1,10 +1,19 @@
 package com.example.shoppingapp
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +25,28 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val retrofitBuilder = Retrofit.Builder()
+            .baseUrl("https://dummyjson.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiInterface::class.java)
+
+        val retrofitData = retrofitBuilder.getProductData()
+
+        retrofitData.enqueue(object : Callback<MyData> {
+            override fun onResponse(p0: Call<MyData>, p1: Response<MyData>) {
+                var responseBody = p1.body()
+                val productArray = responseBody?.products
+
+                val tVProduct = findViewById<TextView>(R.id.tvName)
+                tVProduct.text = productArray.toString()
+            }
+
+            override fun onFailure(p0: Call<MyData>, p1: Throwable) {
+                Log.d(TAG, "onFailure: " + p1.message)
+            }
+
+        })
     }
 }
